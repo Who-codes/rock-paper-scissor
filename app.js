2b770803cc45635272451a952ab6ed7011c012fa
Sulav <sulavshrestha023@gmail.com>
2022-03-09 19:52:40 +0545

    Image issue fixed properly

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ function computerPlay() {
 }
 
 function setImg(player, computer) {
-  playerCard.src = `/images/${player.toLowerCase()}.svg`;
-  computerCard.src = `/images/${computer.toLowerCase()}.svg`;
+  playerCard.src = `./images/${player.toLowerCase()}.svg`;
+  computerCard.src = `./images/${computer.toLowerCase()}.svg`;
 }
 
 function play(player, computer) {
